60881f2f8f7ff81dfd7ae18e0afa679c1c183dc3
Franklin Ikeh <ikehfranklin06@gmail.com>
2026-06-04 14:47:54 +0100

    fix: response field typo in forms table

--- app/features/form/models.py
+++ app/features/form/models.py
@@ -37,7 +37,7 @@ class Form(BaseTableModel):
     user: Mapped["User"] = relationship(back_populates="forms")  # noqa: F821
     user_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
 
-    reponses: Mapped[List["Response"]] = relationship(back_populates="form")
+    responses: Mapped[List["Response"]] = relationship(back_populates="form")
 
     def __str__(self) -> str:
         return "Form: {} - [{}]".format(self.title, self.id)
