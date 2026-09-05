56c07ff092dd964c2abb69b48f84ef686965c41d
Franklin Ikeh <ikehfranklin06@gmail.com>
2026-09-05 14:10:28 +0100

    refactor(password): use bcrypt directly without passlib
    - to remove deprecation warnings

--- app/features/auth/service.py
+++ app/features/auth/service.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Session
 from app.core.logger import logger
 from app.features.auth import schemas as auth_schemas
 from app.features.auth.models import User
-from app.features.auth.utils.password import hash_password, verify_password
 from app.features.auth.repository import UserRepository
+from app.features.auth.utils.password import hash_password, verify_password
 
 
 class UserService:
@@ -37,7 +37,7 @@ class UserService:
                 detail="Invalid email",
             )
 
-        if not verify_password(schema.password, user.password):
+        if user.password and not verify_password(schema.password, user.password):
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Invalid password",
